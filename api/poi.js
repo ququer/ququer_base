7059139d49138df543d32f81e49174e066c9e450
zeka <zengkaizy@163.com>
2014-08-19 20:41:04 +0800

    add files

---
+++ api/poi.js
@@ -0,0 +1,344 @@
+'use strict';
+/*global require, module, Buffer, jsGen*/
+
+var msg = jsGen.lib.msg,
+    MD5 = jsGen.lib.tools.MD5,
+    each = jsGen.lib.tools.each,
+    union = jsGen.lib.tools.union,
+    resJson = jsGen.lib.tools.resJson,
+    toArray = jsGen.lib.tools.toArray,
+    checkID = jsGen.lib.tools.checkID,
+    intersect = jsGen.lib.tools.intersect,
+    filterPoi = jsGen.lib.tools.filterPoi,
+    removeItem = jsGen.lib.tools.removeItem,
+    errorHandler = jsGen.lib.tools.errorHandler,
+    paginationList = jsGen.lib.tools.paginationList,
+    poiDao = jsGen.dao.poi,
+    redis = jsGen.lib.redis,
+    then = jsGen.module.then,
+    poiCache = jsGen.cache.poi,
+    cache = jsGen.lib.redis.poiCache,
+    paginationCache = jsGen.cache.pagination;
+
+poiCache.getP = function (ID) {
+    var that = this,
+        inCache = false;
+
+    return then(function (defer) {
+        if (ID >= 0) {
+            var poi = that.get(ID);
+            if (poi) {
+                inCache = true;
+                return defer(null, poi);
+            } else {
+                return poiDao.getPoi(ID, defer);
+            }
+        } else {
+            defer(jsGen.Err(msg.POI.poiNone));
+        }
+    }).then(function (defer, poi) {
+        if (!inCache) {
+            that.put(ID, poi);
+        }
+        defer(null, poi);
+    }).fail(errorHandler);
+};
+
+
+function convertPois(IDArray, idd) {
+    return then.each(toArray(IDArray), function (defer, x) {
+        cache(x, function (err, poi) {
+            poi = poi && {
+                _id: poiDao.convertID(poi._id),
+                poi: poi.poi,
+                articles: poi.articles,
+                users: poi.users
+            };
+            defer(null, poi || null);
+        });
+    }).all(function (defer, err, list) {
+        removeItem(list, null);
+        defer(null, list);
+    });
+}
+
+function setPoi(poiObj) {
+    var setKey = null;
+
+    return then(function (defer) {
+        if (!poiObj || !poiObj._id) {
+            defer(true);
+        } else if (poiObj.poi) {
+            setKey = 'poi';
+        } else if (poiObj.articlesList) {
+            setKey = 'articlesList';
+        } else if (poiObj.usersList) {
+            setKey = 'usersList';
+        }
+        cache(poiObj._id, defer);
+    }).then(function (defer, poi) {
+        if (setKey === 'poi') {
+            then(function (defer2) {
+                if (poiObj.poi === poi.poi) {
+                    defer(true);
+                } else {
+                    cache.get(poiObj.poi, defer2);
+                }
+            }).all(function (defer2, err, ID) {
+                if (!err && ID !== poiObj._id) {
+                    defer2(null, ID);
+                } else {
+                    defer2(true);
+                }
+            }).then(function (defer2, toID) {
+                poiCache.getP(poiObj._id).then(function (defer3, poi) {
+                    then.each(poi.articlesList, function (defer4, x) {
+                        if (x) {
+                            setPoi({
+                                _id: toID,
+                                articlesList: x
+                            });
+                            jsGen.cache.list.getP(x, false).then(function (defer5, article) {
+                                removeItem(article.poisList, poiObj._id);
+                                if (article.poisList.indexOf(toID) < 0) {
+                                    article.poisList.push(toID);
+                                    jsGen.cache.list.put(article._id, article);
+                                    jsGen.cache.article.update(article._id, function (value) {
+                                        value.poisList = article.poisList;
+                                        return value;
+                                    });
+                                    jsGen.dao.article.setArticle({
+                                        _id: x,
+                                        poisList: article.poisList
+                                    });
+                                }
+                            });
+                        }
+                        defer4();
+                    }).each(poi.usersList, function (defer4, x) {
+                        if (x) {
+                            setPoi({
+                                _id: toID,
+                                usersList: x
+                            });
+                            jsGen.cache.user.getP(x, false).then(function (defer5, user) {
+                                removeItem(user.poisList, poiObj._id);
+                                if (user.poisList.indexOf(toID) < 0) {
+                                    user.poisList.push(toID);
+                                    jsGen.cache.user.put(user._id, user);
+                                    jsGen.dao.user.setUserInfo({
+                                        _id: x,
+                                        poisList: user.poisList
+                                    });
+                                }
+                            });
+                        }
+                        defer4();
+                    });
+                    poiDao.delPoi(poiObj._id, defer3);
+                }).then(function (defer3) {
+                    poiCache.remove(poiObj._id);
+                    cache.remove(poiObj._id);
+                    poiCache.getP(toID).all(defer);
+                }).fail(defer);
+            }, function (defer2) {
+                poiDao.setPoi(poiObj, function (err, poi) {
+                    if (poi) {
+                        cache.update(poi);
+                        poiCache.put(poi._id, poi);
+                    }
+                    defer(err, poi);
+                });
+            }).fail(defer);
+        } else if (setKey === 'articlesList' || setKey === 'usersList') {
+            poiCache.getP(poiObj._id).then(function (defer2, poi) {
+                var exist = poi[setKey].indexOf(Math.abs(poiObj[setKey]));
+                if ((poiObj[setKey] < 0 && exist >= 0) || (poiObj[setKey] > 0 && exist < 0)) {
+                    poiDao.setPoi(poiObj, defer2);
+                } else {
+                    defer2(true);
+                }
+            }).then(function (defer2, poi) {
+                cache.update(poi);
+                poiCache.put(poi._id, poi);
+            }).fail(defer);
+        } else {
+            defer(true);
+        }
+    }).fail(function (defer, err) {
+        defer(err === true ? jsGen.Err(msg.MAIN.requestDataErr) : err);
+    });
+}
+
+function filterPois(poiArray) {
+    return then.each(toArray(poiArray), function (defer, x) {
+        if (x && (x = filterPoi(x))) {
+            then(function (defer2) {
+                cache.get(x, defer2);
+            }).then(function (defer2, ID) {
+                defer(null, ID);
+            }, function (defer2, err) {
+                poiDao.setNewPoi({
+                    poi: x
+                }, function (err, poi) {
+                    defer(null, poi ? (cache.update(poi), poi._id) : null);
+                });
+            });
+        } else {
+            defer(null, null);
+        }
+    }).then(function (defer, IDArray) {
+        removeItem(IDArray, null);
+        defer(null, IDArray);
+    });
+}
+
+function getPoiID(req) {
+    var poi = decodeURI(req.path[2]);
+    return then(function (defer) {
+        if (checkID(poi, 'P')) {
+            defer(null, poiDao.convertID(poi));
+        } else {
+            cache.get(poi, function (err, ID) {
+                defer(err ? jsGen.Err(msg.POI.poiNone) : null, ID);
+            });
+        }
+    }).then(function (defer, ID) {
+        console.log(ID);
+        cache(ID, defer);
+    }).fail(errorHandler);
+}
+
+function getPoi(req, res) {
+    var poi,
+        p = +req.getparam.p || +req.getparam.pageIndex || 1;
+    console.log('getPoi 0');
+    req.session.paginationKey = req.session.paginationKey || {};
+    console.log('getPoi 1');
+    getPoiID(req).then(function (defer, doc) {
+        console.log('getPoi 2');
+        var key = 'Poi' + doc.poi,
+            list = paginationCache.get(req.session.paginationKey[key]);
+        console.log('getPoi key： ' + key);
+        poi = doc;
+        if (!list || p === 1) {
+            then(function (defer2) {
+                poiCache.getP(poi._id).all(defer2);
+            }).then(function (defer2, poi) {
+                list = poi.articlesList;
+                req.session.paginationKey[key] = MD5(JSON.stringify(list.slice(0, 100)), 'base64');
+                paginationCache.put(req.session.paginationKey[key], list);
+                defer(null, list);
+            }).fail(defer);
+        } else {
+            defer(null, list);
+        }
+    }).then(function (defer, list) {
+        paginationList(req, list, jsGen.cache.list, defer);
+    }).then(function (defer, data, pagination) {
+        poi._id = poiDao.convertID(poi._id);
+        console.log('getPoi key： ' + poi._id);
+        return res.sendjson(resJson(null, data, pagination, {
+            poi: poi
+        }));
+    }).fail(res.throwError);
+}
+
+function getPois(req, res) {
+    var list,
+        s = +req.path[3],
+        p = +req.getparam.p || +req.getparam.pageIndex || 1,
+        listPagination = req.session.listPagination;
+
+    then(function (defer) {
+        cache.index(0, -1, defer);
+    }).then(function (defer, list) {
+        paginationList(req, list, poiCache, defer);
+    }).then(function (defer, data, pagination) {
+        each(data, function (poi) {
+            poi._id = poiDao.convertID(poi._id);
+            delete poi.articlesList;
+            delete poi.usersList;
+        });
+        return res.sendjson(resJson(null, data, pagination));
+    }).fail(res.throwError);
+}
+
+function editPois(req, res) {
+    var defaultObj = {
+        _id: '',
+        poi: ''
+    },
+        result = {};
+
+    then(function (defer) {
+        if (!req.session.role || req.session.role < 4) {
+            defer(jsGen.Err(msg.USER.userRoleErr));
+        } else {
+            defer(null, toArray(req.apibody.data));
+        }
+    }).each(null, function (defer, x) {
+            x = intersect(union(defaultObj), x);
+            x._id = poiDao.convertID(x._id);
+            setPoi(x).all(function (defer, err, poi) {
+                if (poi) {
+                    poi._id = poiDao.convertID(poi._id);
+                    delete poi.articlesList;
+                    delete poi.usersList;
+                    result[poi._id] = poi;
+                }
+                defer();
+            });
+    }).then(function (defer) {
+        res.sendjson(resJson(null, result));
+    }).fail(res.throwError);
+}
+
+function delPoi(req, res) {
+    var ID;
+    getPoiID(req).then(function (defer, poi) {
+        if (req.session.role !== 5) {
+            defer(jsGen.Err(msg.USER.userRoleErr));
+        } else {
+            ID = poi._id;
+            poiDao.delPoi(ID, defer);
+        }
+    }).then(function (defer) {
+        poiCache.remove(ID);
+        cache.remove(ID);
+        return res.sendjson(resJson());
+    }).fail(res.throwError);
+}
+
+
+
+module.exports = {
+    GET: function (req, res) {
+        console.log('poi.js: ' + req.path[2]);
+        switch (req.path[2]) {
+        case undefined:
+        case 'index':
+        case 'hots':
+            return getPois(req, res);
+        default:
+            return getPoi(req, res);
+        }
+    },
+    POST: function (req, res) {
+        switch (req.path[2]) {
+        case undefined:
+        case 'index':
+            return getPois(req, res);
+        case 'admin':
+            return editPois(req, res);
+        default:
+            return res.r404();
+        }
+    },
+    DELETE: function (req, res) {
+        return delPoi(req, res);
+    },
+    filterPois: filterPois,
+    setPoi: setPoi,
+    convertPois: convertPois
+};
---
+++ dao/poiDao.js
@@ -0,0 +1,205 @@
+'use strict';
+/*global require, module, Buffer, jsGen*/
+
+/*
+    convertID(id);
+    getPoisNum(callback);
+    getLatestId(callback);
+    getPoisList(_idArray, callback);
+    getPoi(_id, callback);
+    setPoi(poiObj, callback);
+    setNewPoi(poiObj, callback);
+    delPoi(_idArray, callback);
+ */
+var noop = jsGen.lib.tools.noop,
+    union = jsGen.lib.tools.union,
+    intersect = jsGen.lib.tools.intersect,
+    IDString = jsGen.lib.json.IDString,
+    defautPoi = jsGen.lib.json.Poi,
+    callbackFn = jsGen.lib.tools.callbackFn,
+    wrapCallback = jsGen.lib.tools.wrapCallback,
+    converter = jsGen.lib.converter,
+    pois = jsGen.dao.db.bind('pois');
+
+pois.bind({
+
+    convertID: function (id) {
+        switch (typeof id) {
+        case 'string':
+            id = id.substring(1);
+            return converter(id, 62, IDString);
+        case 'number':
+            id = converter(id, 62, IDString);
+            while (id.length < 3) {
+                id = '0' + id;
+            }
+            return 'P' + id;
+        default:
+            return null;
+        }
+    },
+
+    getPoisNum: function (callback) {
+        this.count(wrapCallback(callback));
+    },
+
+    getLatestId: function (callback) {
+        callback = callback || callbackFn;
+        this.findOne({}, {
+            sort: {
+                _id: -1
+            },
+            hint: {
+                _id: 1
+            },
+            fields: {
+                _id: 1
+            }
+        }, callback);
+    },
+
+    getPoisIndex: function (callback) {
+        callback = callback || callbackFn;
+        this.find({}, {
+            sort: {
+                _id: 1
+            },
+            hint: {
+                _id: 1
+            },
+            fields: {
+                _id: 1,
+                poi: 1,
+                articles: 1,
+                users: 1
+            }
+        }).each(callback);
+    },
+
+    getPoi: function (_id, callback) {
+        this.findOne({
+            _id: +_id
+        }, {
+            sort: {
+                _id: -1
+            },
+            fields: {
+                poi: 1,
+                articles: 1,
+                articlesList: 1,
+                users: 1,
+                usersList: 1
+            }
+        }, wrapCallback(callback));
+    },
+
+    setPoi: function (poiObj, callback) {
+        var setObj = {},
+            newObj = {
+                poi: '',
+                articlesList: 0,
+                usersList: 0
+            };
+
+        newObj = intersect(newObj, poiObj);
+        if (newObj.poi) {
+            setObj.$set = {
+                poi: newObj.poi
+            };
+        } else if (newObj.articlesList) {
+            if (newObj.articlesList < 0) {
+                newObj.articlesList = -newObj.articlesList;
+                setObj.$inc = {
+                    articles: -1
+                };
+                setObj.$pull = {
+                    articlesList: newObj.articlesList
+                };
+            } else {
+                setObj.$inc = {
+                    articles: 1
+                };
+                setObj.$push = {
+                    articlesList: newObj.articlesList
+                };
+            }
+        } else if (newObj.usersList) {
+            if (newObj.usersList < 0) {
+                newObj.usersList = -newObj.usersList;
+                setObj.$inc = {
+                    users: -1
+                };
+                setObj.$pull = {
+                    usersList: newObj.usersList
+                };
+            } else {
+                setObj.$inc = {
+                    users: 1
+                };
+                setObj.$push = {
+                    usersList: newObj.usersList
+                };
+            }
+        }
+
+        if (callback) {
+            this.findAndModify({
+                _id: poiObj._id
+            }, [], setObj, {
+                w: 1,
+                'new': true
+            }, wrapCallback(callback));
+        } else {
+            this.update({
+                _id: poiObj._id
+            }, setObj, noop);
+        }
+    },
+
+    setNewPoi: function (poiObj, callback) {
+        var that = this,
+            poi = union(defautPoi),
+            newPoi = union(defautPoi);
+        callback = callback || callbackFn;
+
+        newPoi = intersect(newPoi, poiObj);
+        newPoi = union(poi, newPoi);
+
+        this.getLatestId(function (err, doc) {
+            if (err) {
+                return callback(err, null);
+            }
+            if (!doc) {
+                newPoi._id = 1;
+            } else {
+                newPoi._id = doc._id + 1;
+            }
+            that.findAndModify({
+                _id: newPoi._id
+            }, [], newPoi, {
+                w: 1,
+                upsert: true,
+                'new': true
+            }, wrapCallback(callback));
+        });
+    },
+
+    delPoi: function (_id, callback) {
+        this.remove({
+            _id: +_id
+        }, {
+            w: 1
+        }, wrapCallback(callback));
+    }
+});
+
+module.exports = {
+    convertID: pois.convertID,
+    getPoisNum: pois.getPoisNum,
+    getLatestId: pois.getLatestId,
+    getPoisIndex: pois.getPoisIndex,
+    getPoi: pois.getPoi,
+    setPoi: pois.setPoi,
+    setNewPoi: pois.setNewPoi,
+    delPoi: pois.delPoi
+};
